9637cdfc61fa04664f4cd1631e0a29977ec83212
Jonathan B. Coe <jonathanbcoe@gmail.com>
2026-02-22 23:11:33 +0000

    Add script to demonstrate out-of-project type checks

--- tests/test_add.py
+++ tests/test_add.py
@@ -1,23 +1,23 @@
-import ty_generated_typestubs
+from ty_generated_typestubs import add as tadd
 import pytest
 
 
 def test_add():
-    assert ty_generated_typestubs.add(1, 2) == 3
+    assert tadd(1, 2) == 3
 
 
 def test_add_negative():
-    assert ty_generated_typestubs.add(-1, -2) == -3
+    assert tadd(-1, -2) == -3
 
 
 def test_add_zero():
-    assert ty_generated_typestubs.add(0, 0) == 0
+    assert tadd(0, 0) == 0
 
 
 def test_add_mixed():
-    assert ty_generated_typestubs.add(1, -1) == 0
+    assert tadd(1, -1) == 0
 
 
 def test_add_type_error():
     with pytest.raises(TypeError):
-        ty_generated_typestubs.add("a", "b")
+        tadd("a", "b")
